b21c928e21225cccbcc2f366833f2ca4ecc5bc6d
Robert Antonius <robert.antonius23@gmail.com>
2024-12-04 13:28:39 +0700

    Maybe it works

--- api/index.py
+++ api/index.py
@@ -1,12 +1,32 @@
 from flask import Flask
 import cv2
 import pymupdf
+import logging
+from api.config import load_configurations, configure_logging
+from .views import webhook_blueprint
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configurations and logging settings
+    load_configurations(app)
+    configure_logging()
+
+    # Import and register blueprints, if any
+    app.register_blueprint(webhook_blueprint)
+
+    return app
+
+app = create_app()
 image_dpi = 72
 image_size_landscape = (1200, 1600)
 image_size_portrait = (1600, 1200)
 blur_threshold = 100 # for now
+
+if __name__ == "__main__":
+    logging.info("Flask app started")
+    app.run(host="0.0.0.0", port=8000)
 
 @app.route('/')
 def home():
@@ -21,7 +41,7 @@ def variance_of_laplacian(image):
     height, width, _ = gray.shape
     if height > width:
         resized = cv2.resize(gray, image_size_portrait)
-    else 
+    else:
         resized = cv2.resize(gray, image_size_landscape)
     return cv2.Laplacian(resized, cv2.CV_64F).var()
 
@@ -29,26 +49,26 @@ def is_blur(image):
     fm = variance_of_laplacian(image)
     return True if fm >= blur_threshold else False
 
-def webhook():
-    # WhatsApp logic to get media
-    #####
-    # If it is a PDF file
-    if(False):
-        ##### Load the document
-        doc = file
-        for i in range(0, doc.page_count()):
-            page = doc.load_page(i)
-            pixmap = page.get_pixmap(dpi=image_dpi)
-            image = pixmap.tobytes()
-            if is_blur(image):
-                ##### Send message
-            else:
-                ##### Send other message
-    # If it is a JPEG or PNG file
-    else:
-        ##### Load the image
-        image = file
-        if is_blur(image):
-            ##### Send message
-        else:
-            ##### Send other message
+# def webhook():
+#     # WhatsApp logic to get media
+#     #####
+#     # If it is a PDF file
+#     if(False):
+#         ##### Load the document
+#         doc = file
+#         for i in range(0, doc.page_count()):
+#             page = doc.load_page(i)
+#             pixmap = page.get_pixmap(dpi=image_dpi)
+#             image = pixmap.tobytes()
+#             if is_blur(image):
+#                 ##### Send message
+#             else:
+#                 ##### Send other message
+#     # If it is a JPEG or PNG file
+#     else:
+#         ##### Load the image
+#         image = file
+#         if is_blur(image):
+#             ##### Send message
+#         else:
+#             ##### Send other message
