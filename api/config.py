960f45b4ffcc35dc9b20cf20305ed190b26a6136
Robert Antonius <robert.antonius23@gmail.com>
2024-12-04 20:15:14 +0700

    Update access token

--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ import sys
 import logging
 
 def load_configurations(app):
-    app.config["ACCESS_TOKEN"] = "EAAVkCDn8mhwBOyIW5pZCXTEVSYcg7U7ELHdnP3zOsvpUXG908L4sUoSJnxlt69mSx0CmaoiqZBLJ8eB688JminXZAhCuCGbZAR48jdhI39XZBimYCFye7aDbZAuTlTisLtRnhQvhhl6JZCQZBeCNzcBgEFUOU6ueWVFWJoqIIFZApMlTKEBim5WKGk340V7iHQSUWWTzuWsHFBy3NGWQpN8vcOMndnTMZD"
+    app.config["ACCESS_TOKEN"] = "EAAVkCDn8mhwBOyUrtXQn0yJsd3GKEggxbw8QqKtqt8qzSxFZBYHn8s7N8I65RbHvv0pXLLhh5TJNQ6qqgOIS289stvq6i95bHolnX6GftBkZBqRPT3OoZBdTQo7BSwiHJQsDmS8ZBLzOnxmF4VsqHdz9CtE89ReBajjbjv56qLt9M0bmZAw6aYbnItlUHaBiF"
     app.config["APP_ID"] = "1517361378925084"
     app.config["APP_SECRET"] = "ba1fa5a0d87d3161d6a6556c71252345"
     app.config["RECIPIENT_WAID"] = "6281278989888"
