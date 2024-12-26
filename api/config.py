b115bf203b010496bc0055ff587eef4ba3ce3188
Robert Antonius <robert.antonius23@gmail.com>
2024-12-26 16:32:32 +0700

    Update config.py

--- api/config.py
+++ api/config.py
@@ -2,12 +2,12 @@ import sys
 import logging
 
 def load_configurations(app):
-    app.config["ACCESS_TOKEN"] = "EAAVkCDn8mhwBOyUrtXQn0yJsd3GKEggxbw8QqKtqt8qzSxFZBYHn8s7N8I65RbHvv0pXLLhh5TJNQ6qqgOIS289stvq6i95bHolnX6GftBkZBqRPT3OoZBdTQo7BSwiHJQsDmS8ZBLzOnxmF4VsqHdz9CtE89ReBajjbjv56qLt9M0bmZAw6aYbnItlUHaBiF"
+    app.config["ACCESS_TOKEN"] = "EAAVkCDn8mhwBO3o1DV28vQfjksG13nOGRgTptuBoLKxNNZCGrvVgWiMFgZCZCs4ZAYAVPVNAY6W926CS9g2LnfXWLKZAKM5q9COJNuipeNQd56YCG4dZB1QhxdkX2WdimzgGlEIFFDdvnVd5P48jUBp3rzTkq1IqJZB7LdfA96wzimH5Kh6qZAzZAn1w0ogo5aRwP"
     app.config["APP_ID"] = "1517361378925084"
     app.config["APP_SECRET"] = "ba1fa5a0d87d3161d6a6556c71252345"
     app.config["RECIPIENT_WAID"] = "6281278989888"
     app.config["VERSION"] = "v21.0"
-    app.config["PHONE_NUMBER_ID"] = "520078131183551"
+    app.config["PHONE_NUMBER_ID"] = "510340225497619"
     app.config["VERIFY_TOKEN"] = "jakeisadog"
 
 def configure_logging():
