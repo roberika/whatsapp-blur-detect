632d8b52ef502c83aa7e88bea917b8261f0d0a4c
Robert Antonius <robert.antonius23@gmail.com>
2024-12-27 19:42:30 +0700

    Perjelas feeback

--- api/utils/reply_messages.py
+++ api/utils/reply_messages.py
@@ -5,15 +5,15 @@ def reply_unknown():
 def reply_document_blur(blur_pages): 
     return "Dokumen yang anda kirim memiliki blur pada halaman " + str(blur_pages) + ", mohon ambil ulang gambar pada halaman tersebut"
 def reply_document_clear(): 
-    return "Dokumen yang anda kirim akan kami proses untuk pengecekan"
+    return "Dokumen yang anda kirim diidentifikasi tidak ada blur dan akan kami proses untuk pengecekan"
 def reply_image_blur():
     return "Gambar yang anda kirim memiliki blur, mohon ambil ulang gambar tersebut"
 def reply_image_clear():
-    return "Gambar dokumen yang anda kirim akan kami proses untuk pengecekan"
+    return "Gambar dokumen yang anda kirim diidentifikasi tidak ada blur dan akan kami proses untuk pengecekan"
 def reply_document_blur_too_long(blur_pages): 
     return "Dokumen yang anda kirim memiliki blur pada halaman " + str(blur_pages) + " pada 50 halaman pertama, mohon ambil ulang gambar pada halaman tersebut"
 def reply_document_clear_too_long(): 
-    return "Dokumen yang anda kirim tidak ada blur pada 50 halamanan pertamanya dan akan kami proses untuk pengecekan"
+    return "Dokumen yang anda kirim diidentifikasi tidak ada blur dan  pada 50 halamanan pertamanya dan akan kami proses untuk pengecekan"
 
 
 
