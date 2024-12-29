a3da9752ad3e657afde45125de34272e4739ad74
Robert Antonius <robert.antonius23@gmail.com>
2024-12-30 01:16:06 +0700

    Highlight

--- api/utils/reply_messages.py
+++ api/utils/reply_messages.py
@@ -3,15 +3,15 @@ def reply_text():
 def reply_unknown(): 
     return "Percakapan ini hanya dapat digunakan untuk mengumpulkan dokumen untuk pengecekan. Mohon hubungi kontak anda untuk informasi lebih lanjut."
 def reply_document_blur(blur_pages): 
-    return "Dokumen yang anda kirim memiliki blur pada halaman " + str(blur_pages) + ", mohon ambil ulang gambar pada halaman tersebut"
+    return "Dokumen yang anda kirim memiliki _*blur*_ pada halaman " + str(blur_pages) + ", mohon ambil ulang gambar pada halaman tersebut"
 def reply_document_clear(): 
     return "Dokumen yang anda kirim diidentifikasi tidak ada blur dan akan kami proses untuk pengecekan"
 def reply_image_blur():
-    return "Gambar yang anda kirim memiliki blur, mohon ambil ulang gambar tersebut"
+    return "Gambar yang anda kirim memiliki _*blur*_, mohon ambil ulang gambar tersebut"
 def reply_image_clear():
     return "Gambar dokumen yang anda kirim diidentifikasi tidak ada blur dan akan kami proses untuk pengecekan"
 def reply_document_blur_too_long(blur_pages): 
-    return "Dokumen yang anda kirim memiliki blur pada halaman " + str(blur_pages) + " pada 100 halaman pertama, mohon ambil ulang gambar pada halaman tersebut"
+    return "Dokumen yang anda kirim memiliki _*blur*_ pada halaman " + str(blur_pages) + " pada 100 halaman pertama, mohon ambil ulang gambar pada halaman tersebut"
 def reply_document_clear_too_long(): 
     return "Dokumen yang anda kirim diidentifikasi tidak ada blur pada 100 halamanan pertamanya dan akan kami proses untuk pengecekan"
 
