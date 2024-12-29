dc9ecf92c2b47650920b45fc9f8ea4ae493cfe01
Robert Antonius <robert.antonius23@gmail.com>
2024-12-29 19:50:50 +0700

    Marks messages after identification

--- api/utils/whatsapp_utils.py
+++ api/utils/whatsapp_utils.py
@@ -52,6 +52,17 @@ def get_reply_text_message_input(recipient, text, message_id):
         }
     )
 
+def get_mark(message_id, status_type):
+    return json.dumps(
+            {
+            "messaging_product": "whatsapp",
+            "status": status_type,
+            "message_id": message_id,
+        }
+    )
+
+# does the identification
+
 image_width = 1600 # ukuran gambar standar WhatsApp
 blur_threshold = 39.71 # https://colab.research.google.com/drive/1gkUsybQlNrhDQhLNg0pAwnqINuNSqRvk?usp=sharing
 
@@ -66,14 +77,14 @@ def is_blur(image, index = 1):
 
 def generate_response(message):
     if is_valid_image_message(message):
-        return identify_blur(message["image"]["id"]), get_message_id(message)
+        return identify_blur(message['id'], message["image"]["id"])
     if is_valid_document_message(message):
-        return identify_blur(message["document"]["id"]), get_message_id(message)
+        return identify_blur(message['id'], message["document"]["id"])
     if is_valid_text_message(message):
         return reply_text(), None
     return reply_unknown(), None
 
-def identify_blur(media_id):
+def identify_blur(message_id, media_id):
     media_url, mime_type = retrieve_media_url(media_id)
     data = download_media(media_url, mime_type)
 
@@ -83,23 +94,29 @@ def identify_blur(media_id):
         if under_100:
             if blur_pages:
                 delete_media(media_id)
-                return reply_document_blur(blur_pages)
+                mark_message(message_id, "deleted")
+                return reply_document_blur(blur_pages), message_id
             else:
-                return reply_document_clear()
+                mark_message(message_id, "read")
+                return reply_document_clear(), message_id
         else:
             if blur_pages:
                 delete_media(media_id)
-                return reply_document_blur_too_long(blur_pages)
+                mark_message(message_id, "deleted")
+                return reply_document_blur_too_long(blur_pages), message_id
             else:
-                return reply_document_clear_too_long()
+                mark_message(message_id, "read")
+                return reply_document_clear_too_long(), message_id
     # If it is an image type file
     elif ('image' in mime_type):
         if process_image(data):
             delete_media(media_id)
-            return reply_image_blur()
+            mark_message(message_id, "deleted")
+            return reply_image_blur(), message_id
         else:
-            return reply_image_clear()
-    return reply_unknown()
+            mark_message(message_id, "read")
+            return reply_image_clear(), message_id
+    return reply_unknown(), None
 
 # Returns on what pages the image is blurred in
 def process_document(data):
@@ -124,7 +141,38 @@ def process_image(data):
     image = imdecode(arr, -1)
     return is_blur(image)
 
-# Returns the image in a fucking header
+# interfaces with WhatsApp Cloud API
+
+# Marks image as read
+def mark_message(message_id, status_type):
+    headers = {
+        "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}",
+    }
+    
+    url = f"https://graph.facebook.com/{current_app.config['VERSION']}/{current_app.config['PHONE_NUMBER_ID']}/messages"
+
+    data = get_mark(message_id, status_type)
+
+    try:
+        response = requests.put(
+            url, data=data, headers=headers, timeout=10
+        )  # 10 seconds timeout as an example
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+    except requests.Timeout:
+        logging.error(f"Timeout occurred while marking message as {status_type}")
+        return jsonify({"status": "error", "message": "Request timed out"}), 408
+    except (
+        requests.RequestException
+    ) as e:  # This will catch any general request exception
+        logging.error(f"Request failed due to: {e}")
+        return jsonify({"status": "error", "message": f"Failed to mark message as {status_type}"}), 500
+    else:
+        # Return the image
+        logging.info(f"Status: {response.status_code}")
+        logging.info(f"Message marked as {status_type}")
+        return None
+
+# Deletes the image
 def delete_media(media_id):
     headers = {
         "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}",
@@ -149,35 +197,8 @@ def delete_media(media_id):
         # Return the image
         logging.info(f"Status: {response.status_code}")
         logging.info("Media deleted")
-        return response.json()['success']
+        return None
 
-# Returns the image in a fucking header
-def delete_media(media_id):
-    headers = {
-        "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}",
-    }
-    
-    url = f"https://graph.facebook.com/{current_app.config['VERSION']}/{media_id}"
-
-    try:
-        response = requests.delete(
-            url, headers=headers, timeout=10
-        )  # 10 seconds timeout as an example
-        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-    except requests.Timeout:
-        logging.error("Timeout occurred while deleting media")
-        return jsonify({"status": "error", "message": "Request timed out"}), 408
-    except (
-        requests.RequestException
-    ) as e:  # This will catch any general request exception
-        logging.error(f"Request failed due to: {e}")
-        return jsonify({"status": "error", "message": "Failed to delete media"}), 500
-    else:
-        # Return the image
-        logging.info(f"Status: {response.status_code}")
-        logging.info("Media deleted")
-        return response.json()['success']
-    
 # Returns the image in a fucking header
 def download_media(media_url, mime_type):
     headers = {
@@ -240,7 +261,6 @@ def retrieve_media_url(media_id):
         logging.info("Media url retrieved")
         return url, mime_type
 
-
 def send_message(data):
     headers = {
         "Content-Type": "application/json",
@@ -284,6 +304,7 @@ def process_whatsapp_message(body):
     
     send_message(data)
 
+# message utilities
 
 def is_valid_whatsapp_message(body):
     """
@@ -297,9 +318,6 @@ def is_valid_whatsapp_message(body):
         and body["entry"][0]["changes"][0]["value"].get("messages")
         and body["entry"][0]["changes"][0]["value"]["messages"][0]
     )
-
-def get_message_id(message):
-    return message['id']
 
 def is_valid_image_message(message):
     return (
